cdea0a5fce6ca15ddbba638b65b08a9c5191871e
jwills43 <jwills9191@gmail.com>
2022-11-17 16:36:07 +0000

    home page done

--- components/Search.js
+++ components/Search.js
@@ -2,32 +2,56 @@ import React, { useState } from 'react'
 import './Search.css'
 import SearchIcon from '@mui/icons-material/Search';
 import MicIcon from '@mui/icons-material/Mic';
+import { useNavigate } from 'react-router-dom'
+import { useStateValue } from "./stateProvider";
+import { actionTypes } from "./reducer"
 
+function Search({ hideButtons = false }) {
+  const [{}, dispatch ] = useStateValue()
 
-function Search() {
-
-  const [ input, setInput ] = useState('')
+  const [input, setInput] = useState('')
+  const navigate = useNavigate()
 
   const search = e => {
     e.preventDefault()
 
-    console.log ('submit')
+    console.log('submit')
+
+    dispatch ({
+      type: actionTypes.SET_SEARCH_TERM,
+      term: input
+    })
+    
+
+    
+    navigate(`/Search?${search}`);
+
+
+
   }
 
   return (
-    <form className='search'>
+    <form className="search">
       <div className='search__input'>
         <SearchIcon className='search__inputIcon' />
         <input value={input} onChange={e => setInput(e.target.value)} />
         <MicIcon />
       </div>
 
-      <div className='search__buttons'>
-        <button type='submit' onClick={search} variant='outlined'>Google Search</button>
-        <button variant='outlined'>Im Feeling Lucky</button>
+      {!hideButtons ? (
+        <div className='search__buttons'>
+          <button type='submit' onClick={search} variant='outlined'>Google Search</button>
+          <button variant='outlined'>Im Feeling Lucky</button>
+        </div>
 
-        
-      </div>
+      ) : (
+        <div className='search__buttons'>
+          <button className='searchButtonsHidden' type='submit' onClick={search} variant='outlined'>Google Search</button>
+          <button className='searchButtonsHidden' variant='outlined'>Im Feeling Lucky</button>
+        </div>
+      )}
+
+
     </form>
   )
 }
