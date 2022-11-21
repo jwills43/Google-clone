c146e61d0458b3a741b02979ccb7b092eca0352c
jwills43 <jwills9191@gmail.com>
2022-11-21 13:36:40 +0000

    unfinished

--- components/Search.js
+++ components/Search.js
@@ -3,28 +3,25 @@ import './Search.css'
 import SearchIcon from '@mui/icons-material/Search';
 import MicIcon from '@mui/icons-material/Mic';
 import { useNavigate } from 'react-router-dom'
-import { useStateValue } from "./stateProvider";
-import { actionTypes } from "./reducer"
+import { useStateValue } from "../stateProvider";
+import reducer, { actionTypes } from "../reducer";
+
 
 function Search({ hideButtons = false }) {
-  const [{}, dispatch ] = useStateValue()
+  
 
   const [input, setInput] = useState('')
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const search = e => {
     e.preventDefault()
 
     console.log('submit')
 
-    dispatch ({
-      type: actionTypes.SET_SEARCH_TERM,
-      term: input
-    })
-    
+   
 
     
-    navigate(`/Search?${search}`);
+    navigate(`/search`);
 
 
 
